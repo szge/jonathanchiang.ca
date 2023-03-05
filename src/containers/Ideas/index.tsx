bc54218a8c9ae41063dd3761ee4e28e494ed2eef
Jonathan Chiang <jchiangconan@gmail.com>
2023-03-05 01:12:47 -0500

    added ideas page

--- src/containers/Ideas/index.tsx
+++ src/containers/Ideas/index.tsx
@@ -1,19 +1,62 @@
 import Image from "next/image"
+import { ChatCompletionResponseMessage } from "openai"
 import React from "react"
 
 type Props = {
   errorType?: "NOT_FOUND" | "UNKNOWN"
 }
+
+export type PromptModeType = "philosophical" | "technical"
+export const TechnicalTopics = [
+  "Any topic",
+  "Supervised",
+  "Unsupervised",
+  "Deep learning",
+  "Reinforcement learning",
+  "NLP",
+  "Computer vision",
+  "Robotics",
+  "Medical",
+  "Neuromorphic computing",
+  "CNNs",
+  "RNNs",
+  "LSTM",
+  "Transformer",
+  "Generative"
+] as const
+export type TechnicalTopicType = typeof TechnicalTopics[number]
+
 // Have a LLM generate ideas for AI alignment research
 const IdeasPage: React.FC<Props> = ({ errorType }) => {
-  const [responses, setResponses] = React.useState<string[]>([
-    "Hello hello test test",
-    "4",
-  ])
+  const [responses, setResponses] = React.useState<string[]>([])
+  const [mode, setMode] = React.useState<PromptModeType>("philosophical")
+  const [topic, setTopic] = React.useState<TechnicalTopicType>("Any topic")
 
-  const generateIdea = () => {
-    console.log("clicked");
-    setResponses([...responses, "5"]);
+  async function generateIdea() {
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ mode: mode, topic: topic }),
+      })
+
+      const data = await response.json()
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        )
+      } else {
+        const responseMessage = data.result as ChatCompletionResponseMessage
+        setResponses([...responses, responseMessage.content])
+      }
+    } catch (error: any) {
+      // Consider implementing your own error handling logic here
+      console.error(error)
+      alert(error.message)
+    }
   }
 
   return (
@@ -21,23 +64,137 @@ const IdeasPage: React.FC<Props> = ({ errorType }) => {
       className={`m-auto max-w-4xl bg-white dark:bg-zinc-700 rounded-3xl py-6 px-6 shadow-md`}
     >
       <div className="flex flex-col items-center gap-5">
-        <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">Use a model to generate ideas for AI alignment research!</h2>
-        <a onClick={generateIdea} className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm cursor-pointer select-none">
+        <div className="flex flex-col items-center">
+          <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
+            Use a model to generate ideas for AI alignment research!
+          </h2>
+          <h2 className="italic text-sm text-gray-500 font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
+            Please do not abuse the poor button. Give it time to respond before
+            pressing it again.
+          </h2>
+        </div>
+        <ButtonGroup
+          mode={mode}
+          setMode={setMode}
+          currentTopic={topic}
+          setTopic={setTopic}
+        />
+        <a
+          onClick={generateIdea}
+          className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm cursor-pointer select-none"
+        >
           💡 Generate a New Idea
         </a>
-        {responses.slice(0).reverse().map((response, index) => {
-          return (
-            <div
-              key={index} 
-              className={`w-full h-6 flex flex-row justify-between items-center mb-2 py-8 bg-opacity-60 px-4 max-w-4xl bg-gray-300 rounded-3xl`}
-            >
+        {responses
+          .slice(0)
+          .reverse()
+          .map((response, index) => {
+            return (
+              <div
+                key={index}
+                className={`w-full flex flex-row justify-between items-center mb-2 py-8 bg-opacity-60 px-4 max-w-4xl bg-gray-300 rounded-3xl`}
+              >
                 {response}
-            </div>
-          )
-        })
-        }
+              </div>
+            )
+          })}
       </div>
     </div>
+  )
+}
+
+const ButtonGroup: React.FC<{
+  mode: PromptModeType
+  setMode: React.Dispatch<React.SetStateAction<PromptModeType>>
+  currentTopic: TechnicalTopicType
+  setTopic: React.Dispatch<React.SetStateAction<TechnicalTopicType>>
+}> = (props) => {
+  const childButtons = (
+    <div className="flex flex-wrap gap-2">
+      {props.mode === "philosophical"
+        ? null
+        : TechnicalTopics.map((topic, index) => (
+            <TechnicalButton
+              key={index}
+              selected={topic == props.currentTopic}
+              type={topic}
+              setType={props.setTopic}
+            />
+          ))}
+    </div>
+  )
+
+  if (props.mode === "philosophical") {
+    return (
+      <div className="inline-flex rounded-md shadow-sm" role="group">
+        <button
+          type="button"
+          className="px-4 py-2 text-sm font-medium border border-gray-200 rounded-l-lg bg-gray-100 text-blue-700 z-10 ring-2 ring-blue-700"
+          onClick={() => {
+            props.setMode("philosophical")
+          }}
+        >
+          Philosophical
+        </button>
+        <button
+          type="button"
+          className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md"
+          onClick={() => {
+            props.setMode("technical")
+          }}
+        >
+          Technical
+        </button>
+      </div>
+    )
+  } else {
+    return (
+      <div className="flex flex-col items-center gap-5">
+        <div className="inline-flex rounded-md shadow-sm" role="group">
+          <button
+            type="button"
+            className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg"
+            onClick={() => {
+              props.setMode("philosophical")
+            }}
+          >
+            Philosophical
+          </button>
+          <button
+            type="button"
+            className="px-4 py-2 text-sm font-medium border border-gray-200 rounded-r-md bg-gray-100 text-blue-700 z-10 ring-2 ring-blue-700"
+            onClick={() => {
+              props.setMode("technical")
+            }}
+          >
+            Technical
+          </button>
+        </div>
+        {childButtons}
+      </div>
+    )
+  }
+}
+
+const TechnicalButton: React.FC<{
+  selected: boolean
+  type: TechnicalTopicType
+  setType: React.Dispatch<React.SetStateAction<TechnicalTopicType>>
+}> = (props) => {
+  return (
+    <button
+      type="button"
+      className={
+        props.selected
+          ? "px-4 py-2 text-sm font-medium border border-gray-200 rounded-lg bg-gray-100 text-blue-700 z-10 ring-2 ring-blue-700"
+          : "px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg"
+      }
+      onClick={() => {
+        props.setType(props.type)
+      }}
+    >
+      {props.type}
+    </button>
   )
 }
 
--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,5 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { TechnicalTopicType, PromptModeType } from "@/src/containers/Ideas";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -16,23 +17,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
+  const mode = req.body.mode as PromptModeType || "technical";
+  const topic = req.body.topic as TechnicalTopicType || "Any topic";
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {"role": "user", "content": generatePrompt(mode, topic)}
+      ],
+      temperature: 1,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    res.status(200).json({ result: completion.data.choices[0].message });
   } catch(error: any) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -49,15 +45,64 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 }
 
-function generatePrompt(animal: string) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(mode: PromptModeType = "technical", topic: TechnicalTopicType = "Any topic") {
+  let topic_prompt = "";
+  switch (topic) {
+    case "Supervised":
+      topic_prompt = "Your idea must be related to supervised learning."
+      break;
+    case "Unsupervised":
+      topic_prompt = "Your idea must be related to unsupervised learning."
+      break;
+    case "Deep learning":
+      topic_prompt = "Your idea must be related to deep learning."
+      break;
+    case "Reinforcement learning":
+      topic_prompt = "Your idea must be related to reinforcement learning."
+      break;
+    case "NLP":
+      topic_prompt = "Your idea must be related to natural language processing."
+      break;
+    case "Computer vision":
+      topic_prompt = "Your idea must be related to computer vision."
+      break;
+    case "Robotics":
+      topic_prompt = "Your idea must be related to robotics."
+      break;
+    case "Medical":
+      topic_prompt = "Your idea must be related to medical AI."
+      break;
+    case "Neuromorphic computing":
+      topic_prompt = "Your idea must be related to neuromorphic computing."
+      break;
+    case "CNNs":
+      topic_prompt = "Your idea must be related to convolutional neural networks."
+      break;
+    case "RNNs":
+      topic_prompt = "Your idea must be related to recurrent neural networks."
+      break;
+    case "LSTM":
+      topic_prompt = "Your idea must be related to LSTMs."
+      break;
+    case "Transformer":
+      topic_prompt = "Your idea must be related to the transformer architecture."
+      break;
+    case "Generative":
+      topic_prompt = "Your idea must be related to generative AI."
+      break;
+  }
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return `What is one novel approach or technique that could be developed to ensure that advanced AI systems align with human values and goals, while maintaining safety and avoiding unintended consequences?
+  Consider a ${mode} aspect of AI alignment, rather than ${mode === "philosophical" ? "technical" : "philosophical"}.
+
+  ${
+    mode === "technical"
+      ? topic_prompt
+      : ""
+  }
+
+  Propose a novel approach that goes beyond existing research, proposes many new ideas, and introduces new technical terms.
+  Summarize your idea in a few sentences.
+  Your idea should be extraordinarily crazy and unconventional.
+  `;
 }
